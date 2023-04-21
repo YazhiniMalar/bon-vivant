927148c9ee23c885301328b10878c765357c76e2
YAZH <catrionarowthri@gmail.com>
2023-04-22 02:12:16 +0530

    added new files

---
+++ static/script.js
@@ -0,0 +1,11 @@
+//nav bar
+window.addEventListener("scroll", ()=>{
+	let value=window.scrollY;
+
+    document.getElementById("travel1").style.left = value * 1.5 + "px";
+    document.getElementById("travel2").style.right = value * 1.5 + "px";
+  if(value >50)
+    document.querySelector(".nav_bar").style.display = "flex";
+    else
+    document.querySelector(".nav_bar").style.display = "none";
+});
