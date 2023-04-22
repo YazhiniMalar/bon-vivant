4039096ddf211ea471bf40ebf5c706c2ee3c3d91
YAZH <catrionarowthri@gmail.com>
2023-04-23 00:20:43 +0530

    added few 3d maps

--- static/script.js
+++ static/script.js
@@ -1,9 +1,8 @@
 //nav bar
 window.addEventListener("scroll", ()=>{
 	let value=window.scrollY;
-
     document.getElementById("travel1").style.left = value * 1.5 + "px";
-    document.getElementById("travel2").style.right = value * 1.5 + "px";
+    document.getElementById("travel2").style.left = value * -1.5 + "px";
   if(value >50)
     document.querySelector(".nav_bar").style.display = "flex";
     else
