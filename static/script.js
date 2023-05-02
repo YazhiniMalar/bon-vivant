85b27b25868226f16f323e75145eee86cb13d4b4
YAZH <catrionarowthri@gmail.com>
2023-05-02 12:10:19 +0530

    clg logo & mentor added

--- static/script.js
+++ static/script.js
@@ -4,7 +4,7 @@ window.addEventListener("scroll", ()=>{
     document.getElementById("travel1").style.left = value * 1.5 + "px";
     document.getElementById("travel2").style.left = value * -1.5 + "px";
   if(value >50)
-    document.querySelector(".nav_bar").style.display = "flex";
+    document.querySelector(".navi").style.display = "flex";
     else
-    document.querySelector(".nav_bar").style.display = "none";
+    document.querySelector(".navi").style.display = "none";
 });
